076f50e62e25336f0c50824aba20c5b772e52947
trylobbit <katarzyna.satyla@gmail.com>
2018-02-12 23:45:54 +0100

    porządkowanie

--- src/main/java/com/kasia/domain/products/Product.java
+++ src/main/java/com/kasia/domain/products/Product.java
@@ -1,30 +1,53 @@
 package com.kasia.domain.products;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
 
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private long sn;
     private double price;
     private String clarity;
     private ProductType type;
+    double carat;
+
 
     public Product() {
     }
 
-    public Product(long id, double price, String clarity, ProductType type) {
+
+
+    public Product(long id, long sn, double price, String clarity, ProductType type, double carat) {
         this.id = id;
+        this.sn = sn;
         this.price = price;
         this.clarity = clarity;
         this.type = type;
+        this.carat = carat;
     }
 
     public void setType(String type) {
         this.type = ProductType.valueOf(type);
     }
 
+    public double getCarat() {
+        return carat;
+    }
+
+    public void setCarat(double carat) {
+        this.carat = carat;
+    }
+
+
     public ProductType getType() {
         return type ;
     }
-
     public long getId() {
         return id;
     }
@@ -32,6 +55,15 @@ public class Product {
     public void setId(long id) {
         this.id = id;
     }
+
+    public long getSn() {
+        return sn;
+    }
+
+    public void setSn(long sn) {
+        this.sn = sn;
+    }
+
 
     public double getPrice() {
         return price;
